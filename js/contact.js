e83500cb915534bdbba65fb3c8d867879dab3d9f
Pradeep Prajapati <prajapatipradeep56@gmail.com>
2023-10-16 21:53:07 +0545

    mail service intregrated and blogs apge added

--- js/contact.js
+++ js/contact.js
@@ -4,13 +4,35 @@ document.addEventListener("DOMContentLoaded", function () {
   
     contactForm.addEventListener("submit", async function (e) {
       e.preventDefault();
-  
-      const formData = new FormData(contactForm);
+
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+      const subject = document.getElementById("subject").value;
+      
+      
+      // Create the email data to send to the server
+      const emailData = {
+        name,
+        email,
+        message,
+        subject,
+      }
   
       try {
-        const response = await fetch("https://job-search-git-pra-dev-prajapati-pradeep.vercel.app//send-email", {
+        const response = await fetch("http://localhost:3000/email/contact", {
           method: "POST",
-          body: formData,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(emailData)
+          // body: emailData,
         });
   
         if (response.ok) {
---
+++ js/job-apply.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const contactForm = document.getElementById("job-apply");
+
+  contactForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const attachment = document.getElementById("attachment").files[0]; // Get the selected file
+    const message = document.getElementById("message").value;
+    console.log('name ' + name)
+
+    // Create the email data to send to the server
+    const emailData = new FormData();
+    emailData.append("name", name);
+    emailData.append("email", email);
+    emailData.append("attachment", attachment);
+    emailData.append("message", message);
+
+    const fileSize = attachment.size; // File size in bytes
+    console.log(`File size: ${emailData} bytes`);
+
+    try {
+      const response = await fetch("http://localhost:3000/sendEmail/job-apply", {
+        method: "POST",
+        body: emailData, // Use FormData directly as the body
+      });
+
+      if (response.ok) {
+        // Successful submission, show a success message or redirect
+        alert("Message sent successfully");
+        contactForm.reset();
+      } else {
+        // Handle the error response, e.g., show an error message
+        alert("Message sending failed. Please try again later.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+      alert("An error occurred. Please try again later.");
+    }
+  });
+});
